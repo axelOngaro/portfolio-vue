4da2ac420f4d43a3f0098fce546fb72fbf74569a
Axel Ongaro <axel.ongaro.pro@gmail.com>
2021-02-13 03:51:48 +0100

    Added Technologies section

--- src/Pages/Home.vue
+++ src/Pages/Home.vue
@@ -2,16 +2,19 @@
   <div class="home">
     <Header />
     <Presentation />
+    <Technologies />
   </div>
 </template>
 <script>
 import Header from '../components/Header';
 import Presentation from '../components/Presentation';
+import Technologies from '../components/Technologies.vue';
 export default {
   name: 'Home',
   components: {
     Header,
     Presentation,
+    Technologies,
   },
 };
 </script>
---
+++ src/components/Technologies.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="technologies">
+    <h1>Techno i Use</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Technologies',
+};
+</script>
+
+<style scoped>
+.technologies {
+  background-color: lightskyblue;
+  color: white;
+  height: 100vh;
+}
+</style>
