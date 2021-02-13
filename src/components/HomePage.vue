bb53cbb3c489b8c6914c56d6ce870df23a44525c
Axel Ongaro <axel.ongaro.pro@gmail.com>
2021-02-13 03:38:31 +0100

    Added style resets, boilerplate for homepage

---
+++ src/components/HomePage.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="homepage">
+    <h1>Hello there</h1>
+  </div>
+</template>
+<script>
+export default {
+  name: 'HomePage',
+};
+</script>
+<style scoped>
+.homepage {
+  background-color: lightgray;
+  height: 100vh;
+}
+</style>
